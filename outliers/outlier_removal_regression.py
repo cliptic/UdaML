89825d4da20903240483a5ad03dab14eace921ca
cliptic <50213900+cliptic@users.noreply.github.com>
2019-07-17 20:57:21 +0300

    Outlier regression. code part 1

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,13 +26,11 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
-
-
-
-
-
-
-
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg = reg.fit(ages_train, net_worths_train)
+pred = reg.predict(ages_test)
+print("score for the regression with all training data points:", reg.score(ages_test, net_worths_test))
 
 
 
