975104b49c85ac02d639bb85a331cd297aab815a
cliptic <50213900+cliptic@users.noreply.github.com>
2019-07-18 16:25:07 +0300

    processing emails. assignment3

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -44,30 +44,37 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
+            print(path)
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            email = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            replacables = ["sara", "shackleton", "chris", "germani"]
+            m = 0
+            for i in replacables:
+                email = email.replace(replacables[m], "")
+                m += 1
             ### append the text to word_data
-
+            word_data.append(email)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            if from_person == from_sara:
+                from_data.append(0)
+            if from_person == from_chris:
+                from_data.append(1)
 
+            #email.close()
 
-            email.close()
-
-print "emails processed"
+print("emails processed")
 from_sara.close()
 from_chris.close()
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+pickle.dump( word_data, open("your_word_data.pkl", "wb") )
+pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
 
 
-
+print(word_data[152])
 
 
 ### in Part 4, do TfIdf vectorization here
