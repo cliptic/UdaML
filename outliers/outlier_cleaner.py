5e564571602db7faea4e8f21845b6f4b7305a40c
cliptic <50213900+cliptic@users.noreply.github.com>
2019-07-18 10:36:00 +0300

    Outlier cleaner WORKS

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -13,7 +13,15 @@ def outlierCleaner(predictions, ages, net_worths):
     
     cleaned_data = []
 
+    #create an array of squared errors
+    squared_errors = []
+    n = 0
+    for i in predictions:
+        squared_errors.append((predictions[n] - net_worths[n])**2)
+        n += 1
 
+    # Function returns a list of indexes 
+    # for a percentage of largest elements of a list
     def Nmaxelements(list1, percent): 
         indexes = []
         N = int(len(list1)/100*percent)
@@ -25,12 +33,12 @@ def outlierCleaner(predictions, ages, net_worths):
                     m = j
             list1[m] = 0
             indexes.append(m) 
-
         return indexes
-    print(Nmaxelements(squared_errors, 10))
-        
+    indexlist = Nmaxelements(squared_errors, 10)    
 
-    ### your code goes here
-
+    for i in range(len(predictions)):
+        if i not in indexlist:
+            tuple_n = (ages[i], net_worths[i], squared_errors[i])
+            cleaned_data.append(tuple_n)
     
     return cleaned_data
